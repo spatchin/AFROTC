f0a4d0c3e4cdf697c76c51c869573a3281678f2c
smpatchin <smpatchin@wisc.edu>
2017-01-06 08:59:34 -1000

    add role to user forms and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,6 +66,6 @@ class UsersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:email, :name, :phone_number, :title)
+    params.require(:user).permit(:email, :name, :phone_number, :title, role_ids: [])
   end
 end
