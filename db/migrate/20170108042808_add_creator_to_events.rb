6fecc35680090bfa3fe763641b1a2ed5048baaf6
smpatchin <smpatchin@wisc.edu>
2017-01-16 15:33:17 -0600

    modify creator migration

--- db/migrate/20170108042808_add_creator_to_events.rb
+++ db/migrate/20170108042808_add_creator_to_events.rb
@@ -1,5 +1,5 @@
 class AddCreatorToEvents < ActiveRecord::Migration[5.0]
   def change
-    add_reference :events, :creator, foreign_key: true
+    add_reference :events, :creator, index: true
   end
 end
